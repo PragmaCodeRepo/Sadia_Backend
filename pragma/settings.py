b932864876c52a541fa3c1d6cfa53c90ae99290c
PragmaCodeRepo <chaubeypranjal027@gmail.com>
2023-02-13 17:22:31 +0530

    jh

--- pragma/settings.py
+++ pragma/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 MEDIA_URL='/media/'
 STATIC_ROOT='/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
